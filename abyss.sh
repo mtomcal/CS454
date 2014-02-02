329f6f0e2168a61fcb4b92d9ac109a68bb000beb
Wenyuan Yu <wyu@cn156.(none)>
2014-02-01 16:49:44 -0800

    abyss script

--- abyss.sh
+++ abyss.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#PBS -N name
-#PBS -o /home4/mtomcal/logs
-#PBS -e /home4/mtomcal/logs
-#PBS -d /home4/mtomcal/logs
+#PBS -N abyss
+#PBS -o /home14/wyu/abyss
+#PBS -e /home14/wyu/abyss
+#PBS -d /home14/wyu/abyss
 #PBS -m ae
-#PBS -M mtomcal@uoregon.edu
+#PBS -M wyu@uoregon.edu
 #PBS -l nodes='1':ppn='12'
 #PBS -q fatnodes
 
-dir=/home4/mtomcal
-out=$dir/output
-tmp=/tmp/mtomcal
+dir=/home14/wyu
+out=$dir/abyss/output
+tmp=/tmp/wyu
 
 source $dir/.bash_profile
 
@@ -19,13 +19,16 @@ mkdir -p $out
 
 # Make tmp folder
 mkdir -p $tmp
+module load mpi-tor/openmpi-1.5.4_gcc-4.5.3
 
 # Run Command Below
-$dir/abyss-1.3.7/bin/abyss-pe k=64 name=flower lib='pe1' mp='mp1 mp2 mp3' \
+$dir/abyss-1.3.7/bin/abyss-pe -C $tmp \
+k=64 name=flower lib='pe1' mp='mp1 mp2 mp3' \
 pe1='/home14/mstreis/in/PE_A/PE_fil_R1.fastq /home14/mstreis/in/PE_A/PE_fil_R2.fastq' 
 mp1='/home14/mstreis/in/mate_pair-112613/prc_sr_out_ACAGTG/ACAGTG_R1.fastq /home14/mstreis/in/mate_pair-112613/prc_sr_out_ACAGTG/ACAGTG_R2.fastq' \
 mp2='/home14/mstreis/in/mate_pair-112613/prc_sr_out_GCCAAT/GCCAAT_R1.fastq /home14/mstreis/in/mate_pair-112613/prc_sr_out_GCCAAT/GCCAAT_R2.fastq' \
-mp3='/home14/mstreis/in/mate_pair-112613/prc_sr_out_ATGTCA/ATGTCA_R1.fastq /home14/mstreis/in/mate_pair-112613/prc_sr_out_ATGTCA/ATGTCA_R2.fastq'
+mp3='/home14/mstreis/in/mate_pair-112613/prc_sr_out_ATGTCA/ATGTCA_R1.fastq /home14/mstreis/in/mate_pair-112613/prc_sr_out_ATGTCA/ATGTCA_R2.fastq' \
+
 
 # copy data from scratch (or tmp) directory back to home directory for long term storage:
 /bin/cp -a $tmp/* $out
