d9812cc685433cfdde3707ef1ef5551847df8d83
Michael A Tomcal <mtomcal@uoregon.edu>
2014-03-11 09:55:43 -0700

    Added qsub scaffdense script

---
+++ scaffdense_qsub.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#PBS -N ScaffDense 
+#PBS -l nodes='1':ppn='10'
+#PBS -q generic 
+
+dir=/path/to/scaffdense/folder
+
+source ~/.bash_profile
+
+# Run Command Below
+
+cd $dir
+
+module load python/2.7.6
+module load augustus/2.7
+module load blast
+
+pip install -r --user scaffdense_requirements.txt
+
+python scaffdense.py <scaffold> <blastdb> --threads=10
